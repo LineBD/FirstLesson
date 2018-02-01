85bbb8e030e9805840afe7cf9df735fed31677c3
LineBD <linebdanielsen@gmail.com>
2018-02-01 10:55:11 +0100

    Add project files.

---
+++ Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class CalculatorUnitTest
+    {
+        private HandTestingCalculator.Calculator _uut;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new HandTestingCalculator.Calculator();
+        }
+
+        [Test]
+        public void Add_TwoNumbers_SumIsCorrect()
+        {
+            Assert.That(_uut.Add(2, 3), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Subtract_TwoNumbers_SumIsCorrect()
+        {
+            Assert.That(_uut.Subtract(4,2), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Multiply_TowNumbers_SumIsCorrect()
+        {
+            Assert.That(_uut.Multiply(2,3), Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Power_OnePowerAnotherNumber_SumIsCorrect()
+        {
+            Assert.That(_uut.Power(2,2), Is.EqualTo(4));
+        }
+    }
+}
---
+++ HandTestingCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandTestingCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator myCalculator = new Calculator();
+            var add = myCalculator.Add(2, 3);
+            var subtract = myCalculator.Subtract(4, 2);
+            var multiply = myCalculator.Multiply(2, 3);
+            var power = myCalculator.Power(2,2);
+
+            Console.WriteLine("Add: {0} + {1} = {2}", 2,3, add);
+            Console.WriteLine("Subtract: {0} - {1} = {2}", 4, 2, subtract);
+            Console.WriteLine("Multiply: {0} * {1} = {2}", 2, 3, multiply);
+            Console.WriteLine("Power: {0} ^ {1} = {2}", 2, 2, power);
+
+            Console.ReadKey();
+        }
+    }
+}
